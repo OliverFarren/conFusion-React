ae3a49b1bdb73743164818f6b39e63270f23ffb5
Oliver Farren <oliver.farren@gmail.com>
2020-03-31 10:22:52 +0100

    Single Page Applications P1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
@@ -15,9 +16,11 @@ class App extends Component {
 
   render () {
     return (
-    <div className="App">
-      <Main />
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
