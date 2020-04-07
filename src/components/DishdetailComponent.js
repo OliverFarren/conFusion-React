ee865e247fa41bce54b8ef9075f7ef3496c5702c
Oliver Farren <oliver.farren@gmail.com>
2020-04-07 18:43:01 +0100

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Button, Row, Col, Label, Modal, ModalHeader, ModalBody } from 'reactstr
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -173,7 +174,7 @@ function RenderComments({comments,addComment,dishId}) {
 function RenderDish({dish}) {
     return(
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
